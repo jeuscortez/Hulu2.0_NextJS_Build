ca1aaf6b5e7093964c569af56f6a59196bac1e71
Jesus Cortez <jesuscortez@MacBook-Pro.local>
2021-06-27 19:39:34 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const withAnimations = require('animated-tailwindcss');
+
+module.exports = withAnimations( {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+    animation: ['group-hover']
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+})
